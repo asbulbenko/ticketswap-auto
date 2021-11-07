a0eab8302d443cf327301f1ec6bdb5cb0c4c5854
Alexey Bulbenko <abulbenko@slice.com>
2021-11-07 19:52:13 +0200

    Add logging

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,5 +1,6 @@
 import config.Config;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.After;
 import org.junit.Before;
 import org.openqa.selenium.WebDriver;
@@ -13,6 +14,7 @@ import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+@Slf4j
 public class BaseTest {
     private static final String URL = Config.getAppUrl();
     private static final String EMAIL = Config.getEmail();
@@ -29,6 +31,7 @@ public class BaseTest {
 
     @Before
     public void before() {
+        log.info("Setup a driver");
         WebDriverManager.chromedriver().setup();
         ChromeOptions options = new ChromeOptions();
         options.setHeadless(false);
@@ -36,6 +39,7 @@ public class BaseTest {
         driver.manage().window().maximize();
         service = new GmailService();
 
+        log.info("Start browser and open Ticketswap webpage");
         driver.get(URL);
         loginViaEmailAddress();
         mainPage = new MainPage(driver);
@@ -43,32 +47,36 @@ public class BaseTest {
 
     @After
     public void tearDown() {
-        // Set label Read to unread emails & close driver
+        log.info("Set label Read to unread emails in Gmail inbox");
         service.markAsRead(SUBJECT_EMAIL_FOR_TICKET_SWAP);
+        log.info("Close browser");
         driver.quit();
     }
 
     public void loginViaEmailAddress() {
-        // Open login form and send magicLink to email address
+        log.info("Open login form and send magicLink to email address");
         homePage = new HomePage(driver);
         homePage.openLoginForm();
         homePage.getMagicLink(EMAIL);
 
-        // Open new window and search for email in Gmail inbox
+        log.info("Open new window and search for email in Gmail inbox");
         openMagicLinkFromEmail();
     }
 
     public void openMagicLinkFromEmail() {
         // Get TicketSwap email from Gmail inbox and open magic link
         driver.get(extractUrl());
+        log.info("Magic link opened in browser window");
     }
 
     private String extractUrl() {
+        log.info("Get TicketSwap email from Gmail inbox");
         // Get message body from new email with link
         String response = "";
         int count = 0;
 
         // Wait for email appear in Gmail inbox
+        log.info("Wait for email to appear in Gmail inbox");
         while(!service.isMailExist(SUBJECT_EMAIL_FOR_TICKET_SWAP) || count > 20) {
             count++;
             try {
@@ -76,13 +84,16 @@ public class BaseTest {
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
+            log.debug("Waited {} sec for email to appear", count);
         }
         if(count >= 20 && !service.isMailExist(SUBJECT_EMAIL_FOR_TICKET_SWAP)) {
             throw new RuntimeException("Email doesn't received ");
         }
-
+        log.debug("Get message body from email");
         response = service.emailsBody(SUBJECT_EMAIL_FOR_TICKET_SWAP);
-        // By applying regex find first url in message
+
+        log.info("Get the magic link from received email");
+        log.debug("By applying regex find first url in message");
         List<String> containedUrls = new ArrayList<>();
         String urlRegex = "((https?):((//)|(\\\\))+[\\w\\d:#@%/;$()~_?\\+-=\\\\\\.&]*)";
         Pattern pattern = Pattern.compile(urlRegex, Pattern.CASE_INSENSITIVE);
@@ -92,6 +103,7 @@ public class BaseTest {
             containedUrls.add(response.substring(urlMatcher.start(0),
                     urlMatcher.end(0)));
         }
+        log.debug("Found magic url - {}", containedUrls.get(0));
         return containedUrls.get(0);
     }
 
--- src/test/java/MainTests.java
+++ src/test/java/MainTests.java
@@ -1,14 +1,22 @@
 import config.Config;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TestName;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
+@Slf4j
 public class MainTests extends BaseTest {
+
+    @Rule
+    public TestName name = new TestName();
 
     @Test
     public void testAbleToLoginAndOpenAccount() {
         // Able to open Account
+        log.info("Start test {}", name.getMethodName());
         accountPage = mainPage.openAccountPage();
         assertThat("Username in account page is Oleksii Bulb",
                 accountPage.getFullName(), is("Oleksii Bulb"));
@@ -19,19 +27,25 @@ public class MainTests extends BaseTest {
 
     @Test
     public void ableToSearchEventAddTicketToCartAndDelete() {
+        log.info("Start test {}", name.getMethodName());
+
         // Find Kensigton Artist and open it's event
+        log.info("Find artist Kensington in search field");
         mainPage.searchEvents("Kensington");
-        // Switch get notifications toggle
+        log.info("Switch get notifications toggle");
         mainPage.getNotifiedAboutNewTickets();
-        // Get available type of ticket
+
+        log.info("Get available type of ticket");
         mainPage.getAvailableTicketsType();
         mainPage.getFirstAvailableTicket();
-        // Click buy and proceed to payment
+
+        log.info("Click buy ticket and proceed to payment page");
         cartPage = mainPage.buyTicket();
         cartPage.continueToPaymentButton();
+
         assertThat("Text 'Payment method' absent on page",
                 cartPage.getPaymentTitleText(), is("Payment method"));
-        // Delete tickets from cart
+        log.info("Delete tickets from cart");
         driver.navigate().back();
         cartPage.deleteTicketFromCart();
         assertThat("Text 'Your cart is empty' absent on page",
@@ -40,11 +54,15 @@ public class MainTests extends BaseTest {
 
     @Test
     public void ableToSellTicket() {
+        log.info("Start test {}", name.getMethodName());
         // Customer is able to interact with sell feature
+        log.info("Open sell page & search for artist Kensington to sell");
+        log.info("Add type of tickets");
         mainPage.openSellTicketsForm("Kensington");
         assertThat("Text 'Add your tickets' absent on page",
                 mainPage.getAddTicketPageTitle(), is("Add your tickets"));
-        // empty sell page from added events
+
+        log.info("Navigate back to sell page and delete it");
         driver.navigate().back();
         mainPage.deleteSellEventsFromPage();
     }
